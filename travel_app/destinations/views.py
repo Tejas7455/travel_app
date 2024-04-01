5d552d6aaffa28a316b20c0143f424e61a077d75
Tejas <tkb7455@gmail.com>
2024-04-01 08:26:15 +0530

    post data to save in database from postman app

--- travel_app/destinations/views.py
+++ travel_app/destinations/views.py
@@ -9,3 +9,10 @@ class DestinationAPI(APIView):
         destinations = Destination.objects.all()
         serializer = DestinationSerializers(destinations, many =True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = DestinationSerializers(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data = serializer.data, status=status.HTTP_201_CREATED)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
